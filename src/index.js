70df1761fa2e0b41b98d224b09cc1501ab35e2a4
fellito2903 <fellito29033@hotmail.com>
2021-03-10 01:08:21 -0800

    inicializando restaurante

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+//configuracion
+app.set('port', 3000);
+app.use(morgan('dev')); 
+app.use(express.json());
+
+//rutas
+app.use(require('./routes/ingredient_routes'));
+
+//Empezando el servidor
+app.listen(app.get('port'));
+console.log("inicio de restaurante");
---
+++ src/routes/ingredient_routes.js
@@ -0,0 +1,53 @@
+const { Router } = require('express');
+const router = Router();
+
+//ingredients info
+const ingredients = require('../data/ingredients.json');
+const api_url = '/api/ingredients';
+
+//methods
+function isNameRepeated(new_name) {
+    for (const ingredient of ingredients) {
+        let name_repeated = ingredient.name == new_name;
+        if (name_repeated) return true;
+    }
+    return false;
+}
+
+//get all ingredients
+router.get(api_url, (_, response) => {
+    response.json(ingredients);
+});
+
+//get ingredient by id
+router.get(api_url + '/:id', (request, response) => {
+    const {id} = request.params;
+    ingredients.forEach(ingredient => {
+        if (ingredient.id == id) {
+            response.status(200).json(ingredient);
+            return;
+        }
+    });
+    response.status(500).send('ingredient with id #' + id + ' not found');
+});
+
+//add new ingredient
+router.post(api_url, (request, response) => {
+    const {name, price_kg, price_item} = request.body;
+    
+    if (!(name && price_kg && price_item)) {
+        response.status(500).send('missing data');
+        return;
+    }
+
+    if (isNameRepeated(name)) {
+        response.status(500).send('name is repeated');
+        return;
+    }
+
+    const new_id = ingredients.length + 1;
+    ingredients.push({new_id, name, price_kg, price_item});
+    response.status(200).send('ingredient added successfully');
+});
+
+module.exports = router;
